aa1a0a105279c2853088d918e690941882ae128a
Javier <vegajavier86@gmail.com>
2018-10-26 22:10:20 -0500

    screenshot, mysql install

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,19 @@
 let mysql = require("mysql");
 let figlet = require('figlet');
 let boxen = require('boxen');
-let prompt = require('prompt');
+//let prompt = require('prompt');
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: "localhost",
   
     port: 3306,
   
-    user: "root",
+    user: "root",    
     password: "",
     database: "Bamazon_db"
 });
-
   connection.connect(function(err){
-    if(err) throw err;
+          if(err) throw err;
       start();
   });
 
@@ -27,4 +26,8 @@ figlet("Bamazon", function(err,data ){
     }
     console.log(data)
     console.log(boxen('WELCOME TO BAMAZON', {padding: 1}));
+    console.log (" | Product | Dept Category |  Price  |  Available Inventory  |   " );
+    console.log ("_______________________________________________________________");
+
 });
+
