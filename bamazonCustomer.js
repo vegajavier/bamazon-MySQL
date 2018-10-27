e16c916f049c8c8d1a9ca4878d62f09c71296962
Javier <vegajavier86@gmail.com>
2018-10-27 00:00:41 -0500

    trying to connect mysql code changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,25 +7,35 @@ let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",    
-    password: "",
-    database: "Bamazon_db"
+    password: "@Nthony04192015",
+    database: "bamazon"
 });
   connection.connect(function(err){
           if(err) throw err;
-      start();
-  });
+          console.log("connected");
+          makeTable();
+});
 
-figlet("Bamazon", function(err,data ){
-    if (err){
+let makeTable = function(){
+    connection.query("SELECT * FROM Inventory", function(err,res){
+        for(var i = 0; <res.length; i++){
+            console.log(res [i] .ItemId+ "|") +res[i].ProductName+ "|" +res[i].DepartmentName+ "|" +res[i].price+ "|" +res[i].AvailableInventory+ "\n");
+        }
+    })
+}
+
+
+//figlet("Bamazon", function(err,data ){
+  //  if (err){
         //console.log("not working")
         //Firefox, these function behave quite differently: log only prints out a toString representation, whereas dir prints out a navigable tree. STACKOVERFLOW..
-        console.log(err);
-    }
-    console.log(data)
-    console.log(boxen('WELCOME TO BAMAZON', {padding: 1}));
-    console.log("\n")
-    console.log (" | Product | Dept Category |  Price  |  Available Inventory  |   " );
-    console.log ("_______________________________________________________________");
+    //    console.log(err);
+    //}
+    //console.log(data)
+    //console.log(boxen('WELCOME TO BAMAZON', {padding: 1}));
+    //console.log("\n")
+    //console.log (" | Product | Dept Category |  Price  |  Available Inventory  |   " );
+    //console.log ("_______________________________________________________________");
 
-});
+//});
 
